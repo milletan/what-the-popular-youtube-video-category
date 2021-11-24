cfb8db33790244022023ba08583860deadef9c49
milletan <94892452+milletan@users.noreply.github.com>
2021-11-24 10:45:20 +0800

    Update youtubedata3.py

--- youtubedata3.py
+++ youtubedata3.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 header = st.container()
 dataset = st.container()
@@ -17,8 +18,17 @@ with dataset:
      st.write(youtube_data.head())
      
      st.subheader('Youtube video category with most viewed times')
-     Viewtimes_dist = pd.DataFrame(youtube_data['Viewtimes'].value_counts()).head()
-     st.bar_chart(Viewtimes_dist)
+     fig = plt.figure(figsize=(7,5))
+     
+     names = ["how-to-do","vlog","fitness","cooking","music"]
+     Viewtimes = [1000000, 3000000, 4000000, 200000000, 100000000]
+     positions = [0, 1, 2, 3, 4]
+     
+     plt.bar(positions, Viewtimes, witdh=0.5, color=g)
+     
+     plt.xticks(positions, Viewtimes)
+     
+     plt.show()
      
 with model_training:
     st.header('most popular')
