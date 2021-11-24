38b6126b34407706ded8f0ca4f12b7c948d1204c
milletan <94892452+milletan@users.noreply.github.com>
2021-11-25 06:35:13 +0800

    Update youtubedata3.py

--- youtubedata3.py
+++ youtubedata3.py
@@ -8,11 +8,9 @@ model_training = st.container()
 
 with header:
      st.title('welcome to my page')
-     st.text('in this project i analyse youtube video')
      
 with dataset:
      st.header('youtube dataset')
-     st.text('i created this dataset')
      
      youtube_data = pd.read_csv('https://raw.githubusercontent.com/milletan/what-the-popular-youtube-video-category/main/data%20-%20Sheet1.csv')
      st.write(youtube_data.head())
